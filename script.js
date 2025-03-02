9b9924942ae1c44deeccd5e83550a906fd38089f
Shivam kumar <146973981+Shivam4401@users.noreply.github.com>
2025-03-02 18:52:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+let firstname = document.querySelector("#fname");
+let lastname = document.querySelector("#lname");
+let phone = document.querySelector("#phn");
+let emailid = document.querySelector("#email");
+
+function showAlert(event) {
+	  event.preventDefault();
+	alert(`First Name: ${firstname.value}
+	Last Name: ${lastname.value}
+	Phone Number: ${phone.value}
+	Email ID: ${emailid.value}`)
+}
+
+form.addEventListener("submit",showAlert);
